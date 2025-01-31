fd9aeee18d7a487ce6ebe92e602beb793d2b39e5
Philip Jakob <JakobPh1@schueler.mesk.de>
2025-01-31 21:15:11 +0100

    6.2 fertig gestellt

--- Aufgabe 6/6.2.c
+++ Aufgabe 6/6.2.c
@@ -9,9 +9,7 @@
 //Datenarray, welches als Zwischenspeicher der Methoden fungiert. a, b, radius, phi;
 void einlesen(float dataArray[]) {
 	printf("Welche Koordinaten sollen umgewandelt werden?\n\nKartesische in Polarkoordinaten [1]\nPolarkoordinaten in kartesische [2]\n");
-	int helper = 0;
-	scanf_s("%fl", &dataArray[4]);
-	helper = dataArray[4];
+	scanf_s("%f", &dataArray[4]);
 	if (dataArray[4] == 1) {
 		printf("X und Y Koordinaten eingeben:\n[x],[y]\n");
 		scanf_s("%f,%f", &dataArray[0], &dataArray[1]);
@@ -21,7 +19,7 @@ void einlesen(float dataArray[]) {
 		//Polar in kartesisch
 		printf("Radius und Winkel eingeben:\n[r],[phi]\n");
 		scanf_s("%f,%f", &dataArray[2], &dataArray[3]);
-		//printf("Eingabe war: %.2f,%.2f", dataArray[2], dataArray[3]);
+		printf("Eingabe war: %.2f\t%.2f\n", dataArray[2], dataArray[3]);
 	}
 	else {
 		printf("Eingabe ungültig.\n");
@@ -29,41 +27,48 @@ void einlesen(float dataArray[]) {
 }
 
 void kartesischZuPolar(float dataArray[]) {
-	//Radius bestimmen
-	dataArray[2] = sqrtf(pow(dataArray[0], 2.0) + pow(dataArray[1], 2.0));
-	//Winkel bestimmen
-	//Prüfen ob a oder b gleich null um Berechnung zu überspringen
-	if (dataArray[0] == 0.0f || dataArray[1] == 0.0f) {
-		if (dataArray[0] == 0.0f && dataArray[1] > 0.0f) dataArray[3] = M_PI / 2.0f;			//Rein imaginär-positiv
-		else if (dataArray[0] == 0.0f && dataArray[1] < 0.0f) dataArray[3] = M_PI * 3 / 2.0f;	//Rein imaginär-negativ
-		else if (dataArray[0] == 0.0f && dataArray[1] == 0.0f) dataArray[3] = 0.0f;			//Ursprung
-		else if (dataArray[0] > 0.0f && dataArray[1] == 0.0f) dataArray[3] = 0.0f;				//Rein reell-positiv
-		else if (dataArray[0] < 0.0f && dataArray[1] == 0.0f) dataArray[3] = M_PI;				//Rein reell-negativ			
-	}
-	else {
-		//Ansonsten Winkel berechnen und nach Quadranten korrigieren
-		dataArray[3] = atanf(dataArray[1] / dataArray[0]); //Quadrant I - keine Korrektur
-		if (dataArray[0] > 0.0f && dataArray[1] >= 0.0f) dataArray[3] + 2 * M_PI;	//Quadrant IV
-		else if (dataArray[0] < 0.0f) dataArray[3] + M_PI;			//Quadrant II oder III
+	//Checken ob kartesisch in Polar konvertiert werden soll
+	if (dataArray[4] == 1) {
+		//Radius bestimmen
+		dataArray[2] = sqrtf(pow(dataArray[0], 2.0) + pow(dataArray[1], 2.0));
+		//Winkel bestimmen
+		//Prüfen ob a oder b gleich null um Berechnung zu überspringen
+		if (dataArray[0] == 0.0f || dataArray[1] == 0.0f) {
+			if (dataArray[0] == 0.0f && dataArray[1] > 0.0f) dataArray[3] = M_PI / 2.0f;			//Rein imaginär-positiv
+			else if (dataArray[0] == 0.0f && dataArray[1] < 0.0f) dataArray[3] = M_PI * 3 / 2.0f;	//Rein imaginär-negativ
+			else if (dataArray[0] == 0.0f && dataArray[1] == 0.0f) dataArray[3] = 0.0f;				//Ursprung
+			else if (dataArray[0] > 0.0f && dataArray[1] == 0.0f) dataArray[3] = 0.0f;				//Rein reell-positiv
+			else if (dataArray[0] < 0.0f && dataArray[1] == 0.0f) dataArray[3] = M_PI;				//Rein reell-negativ			
+		}
+		else {
+			//Ansonsten Winkel berechnen und nach Quadranten korrigieren
+			dataArray[3] = atanf(dataArray[1] / dataArray[0]);										//Quadrant I - keine Korrektur
+			if (dataArray[0] > 0.0f && dataArray[1] < 0.0f) dataArray[3] += 2 * M_PI;				//Quadrant IV
+			else if (dataArray[0] < 0.0f) dataArray[3] += M_PI;										//Quadrant II oder III
+		}
 	}
 }
 void polarZuKartesisch(float dataArray[]) {
-	//Mathematik
-	//a = z * cos(phi)
-	//b = z * sin(phi)
-	dataArray[0] = dataArray[2] * cos(dataArray[3]);
-	dataArray[1] = dataArray[2] * sin(dataArray[3]);
+	//Checken ob von Polar zu kartesisch konvertiert werden soll
+	if (dataArray[4] == 2) {
+		//Mathematik
+		//a = z * cos(phi)
+		//b = z * sin(phi)
+		dataArray[0] = dataArray[2] * cos(dataArray[3]);
+		dataArray[1] = dataArray[2] * sin(dataArray[3]);
 	}
+}
 void ausgeben(float dataArray[]) {
 	//Ergebnisausgabe
-	if (dataArray[4] == 1) {
+	float bla = dataArray[4];
+	if (dataArray[4] == 1.0f) {
 		//Polarausgabe
-		printf("Radius:\t%.4f\t\tWinkel (Bogenmass):\t%.4f\n", dataArray[2], dataArray[3]);
+		printf("Radius:\t%.4f\nWinkel (Bogenmass):\t%.4f\n", dataArray[2], dataArray[3]);
 		printf("z = %.2f * cos(%.2f) + i * sin(%.2f)\n", dataArray[2], dataArray[3], dataArray[3]);
 	}
-	else {
+	else if(dataArray[4] == 2.0f){
 		//Kartesische Ausgabe
-		printf("X-Koordinate:\t%f\nY-Koordinate:\t%f\n", dataArray[0], dataArray[1]);
+		printf("\nX-Koordinate:\t%.2f\nY-Koordinate:\t%.2f\n", dataArray[0], dataArray[1]);
 	}
 }
 
