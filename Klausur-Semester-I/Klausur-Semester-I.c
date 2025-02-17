a193129d69fffb776be45c2998ba8ac905d07a5b
Philip Jakob <philip.jakob@gmail.com>
2025-02-18 00:09:24 +0100

    Sync vom Laptop

--- Klausur-Semester-I/Klausur-Semester-I.c
+++ Klausur-Semester-I/Klausur-Semester-I.c
@@ -71,7 +71,7 @@ int num2text(char* text, int maxlen, int num) {
 		backup /= 10;
 		noDigits++;
 	}
-	if (smallnum2text(text, maxlen, num, noDigits) == -1) return -1;	//Zahlen von -9999 bis 9999 ausgeben
+	if (smallnum2text(text, maxlen, num, noDigits) == -1) return -1;	//Zahlen von -2.1Mrd bis +2.1 Mrd ausgeben
 	else return 0;
 }
 /// <summary>
@@ -89,12 +89,9 @@ int smallnum2text(char* text, int maxlen, int num, int digits) {
 	//Edge - Cases
 	if (num == 0) {
 		if (checkStrCat(text, maxlen, woerter.neunzehn[0]) == -1) return -1;
-		/*if (strlen(text) + strlen("null") >= maxlen) return -1;
-		strcat_s(text, maxlen, "null");*/
 	}
 	else if (num < 0) {
-		if (strlen(text) + strlen("minus ") >= maxlen) return -1;
-		strcat_s(text, maxlen, "minus ");
+		if (checkStrCat(text, maxlen, "minus ")== -1) return -1;
 		num *= -1;
 	}
 
@@ -107,8 +104,9 @@ int smallnum2text(char* text, int maxlen, int num, int digits) {
 
 	//Milliarden ausgeben
 	if (digits > 9) {
-		if (strlen(text) + strlen(woerter.neunzehn[zahlenArray[milliarden]]) >= maxlen) return -1;
-		strcat_s(text, maxlen, woerter.neunzehn[zahlenArray[milliarden]]);
+		if (checkStrCat(text, maxlen, woerter.neunzehn[zahlenArray[milliarden]]) == -1) return -1;
+		//if (strlen(text) + strlen(woerter.neunzehn[zahlenArray[milliarden]]) >= maxlen) return -1;		//Milliarden Wert anheften
+		//strcat_s(text, maxlen, woerter.neunzehn[zahlenArray[milliarden]]);
 		if (zahlenArray[milliarden] == 1) {
 			//Gendern wenn 1 Milliarde
 			if (checkStrCat(text, maxlen, "e") == -1) return -1;/*
@@ -152,8 +150,6 @@ int smallnum2text(char* text, int maxlen, int num, int digits) {
 			else if (zahlenArray[millionen] == 1) {
 				//e anhängen wenn 001.XXX.XXX
 				if (checkStrCat(text, maxlen, "e") == -1) return -1;
-				/*if (strlen(text) + strlen("e") >= maxlen) return -1;
-				strcat_s(text, maxlen, "e");*/
 			}
 		}
 		else if (zahlenArray[zehnmillionen] == 1) {
@@ -167,12 +163,12 @@ int smallnum2text(char* text, int maxlen, int num, int digits) {
 			if (zahlenArray[millionen] == 0) {
 				//30m,20m usw abfangen
 				if (checkStrCat(text, maxlen, woerter.zehner[zahlenArray[zehnmillionen]]) == -1) return -1;
-				/*if (strlen(text) + strlen(woerter.zehner[zahlenArray[zehnmillionen]]) >= maxlen) return -1;
-				strcat_s(text, maxlen, woerter.zehner[zahlenArray[zehnmillionen]]);*/
+				//if (strlen(text) + strlen(woerter.zehner[zahlenArray[zehnmillionen]]) >= maxlen) return -1;
+				//strcat_s(text, maxlen, woerter.zehner[zahlenArray[zehnmillionen]]);
 
 			}
 			//millionen überprüfen
-			else if (zahlenArray[millionen] > 1)
+			else if (zahlenArray[millionen] != 0)
 			{
 				//Zahlen 31m,32m,33m
 				if (checkStrCat(text, maxlen, woerter.neunzehn[zahlenArray[millionen]]) == -1) return -1;
@@ -184,20 +180,16 @@ int smallnum2text(char* text, int maxlen, int num, int digits) {
 				//strcat_s(text, maxlen, woerter.zehner[zahlenArray[zehnmillionen]]);	//Zehner printen
 			}
 		}
-		//Postfix -millionen hinzufügen - plural million/millionen abfangen - TODO
+		//Postfix -millionen hinzufügen - plural million/millionen abfangen
 		//million wenn = 001
 		//millionen wenn > 001
-		if (zahlenArray[hundertmillionen] == 0 && zahlenArray[zehnmillionen] == 0 && zahlenArray[millionen] == 1) {
-			if (checkStrCat(text, maxlen, woerter.potenzen[2]) == -1) return -1;
+		if (zahlenArray[hundertmillionen] != 0 && zahlenArray[zehnmillionen] != 0 && zahlenArray[millionen] != 1) {
+			if (checkStrCat(text, maxlen, woerter.potenzen[2]) == -1) return -1;		//postfix million anheften
+			if (zahlenArray[hundertmillionen] != 0 || zahlenArray[zehnmillionen] != 0 || zahlenArray[millionen] > 1) {
+				if (checkStrCat(text, maxlen, "en") == -1) return -1;		//postfix million-en anheften
+			}
 			//if (strlen(text) + strlen(woerter.potenzen[2]) >= maxlen) return -1;
 			//strcat_s(text, maxlen, woerter.potenzen[2]);	//Million anhängen
-		}
-		else if (zahlenArray[hundertmillionen] != 0 || zahlenArray[zehnmillionen] != 0 || zahlenArray[millionen] > 1) {
-			if (checkStrCat(text, maxlen, woerter.potenzen[2]) == -1) return -1;
-			if (checkStrCat(text, maxlen, "en") == -1) return -1;
-			//if (strlen(text) + strlen(woerter.potenzen[2]) + strlen("en") >= maxlen) return -1;
-			//strcat_s(text, maxlen, woerter.potenzen[2]);	//Million anhängen
-			//strcat_s(text, maxlen, "en");					//Pluralform anhängen
 		}
 	}
 
@@ -205,126 +197,81 @@ int smallnum2text(char* text, int maxlen, int num, int digits) {
 	if (zahlenArray[hunderttausend] != 0 || zahlenArray[zehntausend] != 0 || zahlenArray[tausend] != 0) {
 		if (zahlenArray[hunderttausend] != 0) {
 			//Zahlenbereich 100000-900000 - passt
-			if (checkStrCat(text, maxlen, woerter.neunzehn[zahlenArray[hunderttausend]]) == -1) return -1;
-			if (checkStrCat(text, maxlen, woerter.potenzen[0]) == -1) return -1;
-			//if (strlen(text) + strlen(woerter.neunzehn[zahlenArray[hunderttausend]]) + strlen(woerter.potenzen[0]) >= maxlen) return -1;
-			//strcat_s(text, maxlen, woerter.neunzehn[zahlenArray[hunderttausend]]);	//Wert des Hundertausenders anheften
-			//strcat_s(text, maxlen, woerter.potenzen[0]);							//Hundert anheften
+			if (checkStrCat(text, maxlen, woerter.neunzehn[zahlenArray[hunderttausend]]) == -1) return -1; //Hundertausender anheften
+			if (checkStrCat(text, maxlen, woerter.potenzen[0]) == -1) return -1;							//-"Hundert" anheften
 		}
 		if (zahlenArray[zehntausend] == 0 && zahlenArray[tausend] != 0) {
 			//Zahlenbereich 1000-9000	
 			if (checkStrCat(text, maxlen, woerter.neunzehn[zahlenArray[tausend]]) == -1) return -1;
-			/*if (strlen(text) + strlen(woerter.neunzehn[zahlenArray[tausend]]) >= maxlen) return -1;
-			strcat_s(text, maxlen, woerter.neunzehn[zahlenArray[tausend]]);*/
 			if (zahlenArray[hunderttausend] != 0 && zahlenArray[tausend] == 1) {
-				//S anhängen wenn X01000
-				if (checkStrCat(text, maxlen, "s") == -1) return -1;
-				/*if (strlen(text) + strlen("s") >= maxlen) return -1;
-				strcat_s(text, maxlen, "s");*/
+				//S anhängen wenn X01XXX
+				if (checkStrCat(text, maxlen, "s") == -1) return -1;				//Braucht man das?
 			}
 		}
 		else if (zahlenArray[zehntausend] == 1) {
 			//Zahlenbereich 10000-19000
 			if (checkStrCat(text, maxlen, woerter.neunzehn[zahlenArray[tausend] + 10]) == -1) return -1;
-			/*if (strlen(text) + strlen(woerter.neunzehn[zahlenArray[tausend] + 10]) >= maxlen) return -1;
-			strcat_s(text, maxlen, woerter.neunzehn[zahlenArray[tausend] + 10]);*/
 		}
 		else if (zahlenArray[zehntausend] > 1) {
 			//Zahlenbereich 20000-99000
 			if (zahlenArray[tausend] == 0) {
 				//30t,20t usw abfangen
 				if (checkStrCat(text, maxlen, woerter.zehner[zahlenArray[zehntausend]]) == -1) return -1;
-				/*if (strlen(text) + strlen(woerter.zehner[zahlenArray[zehntausend]]) >= maxlen) return -1;
-				strcat_s(text, maxlen, woerter.zehner[zahlenArray[zehntausend]]);*/
-
 			}
 			//Tausender überprüfen
-			else if (zahlenArray[tausend] > 1)
+			else if (zahlenArray[tausend] !=0 )
 			{
 				//Zahlen 31t,32t,33t
-				if (checkStrCat(text, maxlen, woerter.neunzehn[zahlenArray[tausend]]) == -1) return -1;
-				if (checkStrCat(text, maxlen, "und") == -1) return -1;
-				if (checkStrCat(text, maxlen, woerter.zehner[zahlenArray[zehntausend]]) == -1) return -1;
-				
-			//	if (strlen(text) + strlen(woerter.neunzehn[zahlenArray[tausend]]) + strlen("und") + strlen(woerter.zehner[zahlenArray[zehn]]) >= maxlen) return -1;
-			//	strcat_s(text, maxlen, woerter.neunzehn[zahlenArray[tausend]]);	//Einer printen
-			//	strcat_s(text, maxlen, "und");
-			//	strcat_s(text, maxlen, woerter.zehner[zahlenArray[zehntausend]]);	//Zehner printen
-			}
+				if (checkStrCat(text, maxlen, woerter.neunzehn[zahlenArray[tausend]]) == -1) return -1;		//Einer anhängen
+				if (checkStrCat(text, maxlen, "und") == -1) return -1;										//Binde -"und"-
+				if (checkStrCat(text, maxlen, woerter.zehner[zahlenArray[zehntausend]]) == -1) return -1;	//Zehner anhängen
+				}
 		}
 		//Postfix -tausend hinzufügen
-		if (checkStrCat(text, maxlen, woerter.potenzen[1]) == -1) return -1;/*
-		if (strlen(text) + strlen(woerter.potenzen[1]) >= maxlen) return -1;
-		strcat_s(text, maxlen, woerter.potenzen[1]);*/
+		if (checkStrCat(text, maxlen, woerter.potenzen[1]) == -1) return -1;
 	}
 
 	//Hunderter ausgeben - passt
 	if (zahlenArray[hundert] != 0) {
-		if (checkStrCat(text, maxlen, woerter.neunzehn[zahlenArray[hundert]]) == -1) return -1;
-		if (checkStrCat(text, maxlen, woerter.potenzen[0]) == -1) return -1;
-		//if (strlen(text) + strlen(woerter.neunzehn[zahlenArray[hundert]]) + strlen(woerter.potenzen[0]) >= maxlen) return -1;
-		//strcat_s(text, maxlen, woerter.neunzehn[zahlenArray[hundert]]);			//Fügt ein/zwei/drei hinzu
-		//strcat_s(text, maxlen, woerter.potenzen[0]);							//Fügt hundert hinzu
+		//100-900
+		if (checkStrCat(text, maxlen, woerter.neunzehn[zahlenArray[hundert]]) == -1) return -1;	//Hunderter Wert anheften
+		if (checkStrCat(text, maxlen, woerter.potenzen[0]) == -1) return -1;					//Fügt "hundert" hinzu
 	}
 	//Zehner und Einer ausgeben
 	//Wenn Zehner 1 ist, dann 10-19 ausgeben - passt
 	if (zahlenArray[zehn] == 1) {
 		if (checkStrCat(text, maxlen, woerter.neunzehn[zahlenArray[einer] + 10]) == -1) return -1;
-		/*if (strlen(text) + strlen(woerter.neunzehn[zahlenArray[einer] + 10]) >= maxlen) return -1;
-		strcat_s(text, maxlen, woerter.neunzehn[zahlenArray[einer] + 10]);*/
 	}
 	//Ansonsten Zahlenbereich 20-99
 	else if (zahlenArray[zehn] > 1) {
 		if (zahlenArray[einer] == 0) {
 			//30,20 usw abfangen
 			if (checkStrCat(text, maxlen, woerter.zehner[zahlenArray[zehn]]) == -1) return -1;
-			/*if (strlen(text) + strlen(woerter.zehner[zahlenArray[zehn]]) >= maxlen) return -1;
-			strcat_s(text, maxlen, woerter.zehner[zahlenArray[zehn]]);*/
 		}
-		//Einer überprüfen
-		else if (zahlenArray[einer] == 1) //passt
+		else if (zahlenArray[einer] >= 1)	//passt
 		{
-			//Zahlen wie 31, 21 usw abfangen
-			if (checkStrCat(text, maxlen, woerter.neunzehn[zahlenArray[einer]]) == -1) return -1;
-			if (checkStrCat(text, maxlen, "und") == -1) return -1;
-			if (checkStrCat(text, maxlen, woerter.zehner[zahlenArray[zehn]]) == -1) return -1;
-			//if (strlen(text) + strlen(woerter.neunzehn[zahlenArray[einer]]) + strlen("und") + strlen(woerter.zehner[zahlenArray[zehn]]) >= maxlen) return -1;
-			//strcat_s(text, maxlen, woerter.neunzehn[zahlenArray[einer]]);
-			//strcat_s(text, maxlen, "und");
-			//strcat_s(text, maxlen, woerter.zehner[zahlenArray[zehn]]);	//Zehner printen
-		}
-		else if (zahlenArray[einer] > 1)	//passt
-		{
-			//Zahlen 33,34,34
-			if (checkStrCat(text, maxlen, woerter.neunzehn[zahlenArray[einer]]) == -1) return -1;
-			if (checkStrCat(text, maxlen, "und") == -1) return -1;
-			if (checkStrCat(text, maxlen, woerter.zehner[zahlenArray[zehn]]) == -1) return -1;
-
-			//if (strlen(text) + strlen(woerter.neunzehn[zahlenArray[einer]]) + strlen("und") + strlen(woerter.zehner[zahlenArray[zehn]]) >= maxlen) return -1;
-			//strcat_s(text, maxlen, woerter.neunzehn[zahlenArray[einer]]);	//Einer printen
-			//strcat_s(text, maxlen, "und");
-			//strcat_s(text, maxlen, woerter.zehner[zahlenArray[zehn]]);	//Zehner printen
+			//Zahlen 21,32,43,54
+			if (checkStrCat(text, maxlen, woerter.neunzehn[zahlenArray[einer]]) == -1) return -1;	//Einer anheften
+			if (checkStrCat(text, maxlen, "und") == -1) return -1;									//Bindewort -"und"-
+			if (checkStrCat(text, maxlen, woerter.zehner[zahlenArray[zehn]]) == -1) return -1;		//Zehner printen
 		}
 	}
 	else {
 		//Ansonsten Zehner und Einer ausgeben
-		//Zahlenbereich 100-119
-		//Sonderfall 0-9 "und"
 		if (zahlenArray[zehn] == 0 && zahlenArray[einer] != 0 && num > 100) {
-			if (checkStrCat(text, maxlen, "und") == -1) return -1;
-			//if (strlen(text) + strlen(woerter.neunzehn[zahlenArray[einer]]) >= maxlen) return -1;
-			////strcat_s(text, maxlen, "und");
-			//strcat_s(text, maxlen, woerter.neunzehn[zahlenArray[einer]]);
+			//X01-x09
+			if (checkStrCat(text, maxlen, woerter.neunzehn[zahlenArray[einer]]) == -1) return -1;
 			if (zahlenArray[zehn] == 0 && zahlenArray[einer] == 1) {		//Prüfen ob 01 am Ende steht - 11 wird ausgenommen
-				if (checkStrCat(text, maxlen, "s") == -1) return -1;
-				//if (strlen(text) + strlen("s") >= maxlen) return -1;
-				//strcat_s(text, maxlen, "s");								//"s" für eins anhängen	
+				if (checkStrCat(text, maxlen, "s") == -1) return -1;		//s anhängen für 01
 			}
 		}
-		else if (zahlenArray[einer] != 0) {		//Breakpoint gesetzt, aber wahrscheinlich wird dieser Code nie wieder erreicht, durch entfall von "und"
+		else if (zahlenArray[einer] != 0) {		//Breakpoint gesetzt
+			//1-9
 			if (checkStrCat(text, maxlen, woerter.neunzehn[zahlenArray[einer]]) == -1) return -1;
-			/*if (strlen(text) + strlen(woerter.neunzehn[zahlenArray[einer]]) >= maxlen) return -1;
-			strcat_s(text, maxlen, woerter.neunzehn[zahlenArray[einer]]);*/
+			if (zahlenArray[einer] == 1) {
+				//1
+				if (checkStrCat(text, maxlen, "s") == -1) return -1;			//"s" anheften für 1
+			}
 		}
 	}
 	return 0;
